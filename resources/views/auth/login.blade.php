57f15a505fa2f2a76b987024dc3af5677e3c2a09
Dhanuxuu <hansi77dhanu@gmail.com>
2024-10-23 21:09:34 +0530

    add s

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -83,6 +83,6 @@
 <footer class="py-16 text-center text-sm text-black dark:text-white/70">
     <!-- Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }}) -->
     <p class="copyright">&copy; Designed and Developed by Faculty Of Computing.</p>
-    <p>Terms & Conditions | Privacy Policy | Accessibility Statement | Last Updated : Jul 21 2024 | Site Map</p>
+    <p>Terms & Conditionssss | Privacy Policy | Accessibility Statement | Last Updated : Jul 21 2024 | Site Map</p>
 </footer>
 @endsection
