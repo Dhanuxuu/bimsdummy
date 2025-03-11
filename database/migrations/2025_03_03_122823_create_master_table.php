7739ee0f2ed92592309e58eaade38062a9af0a4f
VenumiV <venumivonara@gmail.com>
2025-03-11 11:31:07 +0530

    Changes in master table to database schema

--- database/migrations/2025_03_03_122823_create_master_table.php
+++ database/migrations/2025_03_03_122823_create_master_table.php
@@ -14,22 +14,22 @@ return new class extends Migration
         Schema::create('master', function (Blueprint $table) {
             $table->id();
 
-            $table->unsignedBigInteger('user_email')->nullable();
+            $table->string('user_email')->nullable();
             $table->unsignedBigInteger('blood_id')->nullable();
             $table->unsignedBigInteger('hbb_id')->nullable();
-            $table->unsignedBigInteger('donation_camp_id')->nullable();
+            $table->unsignedBigInteger('camp_id')->nullable();
             $table->unsignedBigInteger('donation_id')->nullable();
             $table->unsignedBigInteger('donor_id')->nullable();
             $table->unsignedBigInteger('blood_transfer_id')->nullable();
             
             // Defining Foreign Key Constraints
             $table->foreign('user_email')->references('email')->on('users')->onDelete('set null');
-            $table->foreign('blood_id')->references('id')->on('blood_inventory')->onDelete('set null');
-            $table->foreign('hbb_id')->references('id')->on('hospital_bb')->onDelete('set null');
+            $table->foreign('blood_id')->references('blood_id')->on('blood_inventory')->onDelete('set null');
+            $table->foreign('hbb_id')->references('hbb_id')->on('hospital_bb')->onDelete('set null');
           //  $table->foreign('HospitalID')->references('id')->on('hospitals')->onDelete('set null');
-            $table->foreign('camp_id')->references('id')->on('donation_camps')->onDelete('set null');
-            $table->foreign('donation_id')->references('id')->on('donations')->onDelete('set null');
-            $table->foreign('donor_id')->references('id')->on('donors')->onDelete('set null');
+            $table->foreign('camp_id')->references('camp_id')->on('donation_camps')->onDelete('set null');
+            $table->foreign('donation_id')->references('donation_id')->on('donations')->onDelete('set null');
+            $table->foreign('donor_id')->references('donor_id')->on('donors')->onDelete('set null');
            // $table->foreign('RequestID')->references('id')->on('blood_requests')->onDelete('set null');
           //  $table->foreign('TransferID')->references('id')->on('blood_transfers')->onDelete('set null');
             $table->timestamps();
